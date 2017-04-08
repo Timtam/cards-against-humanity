897a7e6d893b49ae68a53ac5384dabcccdd68473
Toni Barth <software@satoprogs.de>
2017-04-08 17:04:30 +0200

    added connection accept and client authentification request (since there is no client yet, there is also nothing to authentificate yet) also, implemented a protocol JSONReceiver which is based on the basic LineReceiver but packages all relevant data into one json object and parses the received data into pythonic objects again

--- server/protocol.py
+++ server/protocol.py
@@ -1,4 +1,10 @@
-from twisted.protocols.basic import LineReceiver
+from shared.messages import *
+from shared.protocol import JSONReceiver
 
-class ServerProtocol(LineReceiver):
-  pass
+class ServerProtocol(JSONReceiver):
+  Callbacks = {
+              }
+
+  def connectionMade(self):
+    self.log.info("Connection established from {connection}", connection=self.transport.getPeer())
+    self.sendMessage(MSG_REQUEST_CLIENT_AUTHENTIFICATION)
---
+++ shared/protocol.py
@@ -0,0 +1,19 @@
+import json
+from twisted.logger import Logger
+from twisted.protocols.basic import LineReceiver
+
+class JSONReceiver(LineReceiver):
+  log = Logger()
+
+  def lineReceived(self, line):
+    data = json.loads(line)
+    code = int(data["code"])
+    del(data["code"])
+    self.messageReceived(code, data)
+
+  def messageReceived(self, code, data):
+    pass
+
+  def sendMessage(self, code, data={}):
+    data["code"] = int(code)
+    self.sendLine(json.dumps(data))
