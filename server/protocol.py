c9c82b2e0e8307adc2bb088cf86f006a1d003b9a
Toni Barth <software@satoprogs.de>
2017-04-10 16:42:34 +0200

    rewrote some messages renamed all client stuff to user stuff and added MSG_CLIENT_AUTHENTIFICATION additionally to actually authenticate the client itself also, all messages send to the protocol while the client did not actually get authenticated will result in an error

--- server/protocol.py
+++ server/protocol.py
@@ -6,24 +6,31 @@ from shared.protocol import JSONReceiver
 class ServerProtocol(JSONReceiver):
 
   def __init__(self,factory):
+    self.authenticated=False
     self.callbacks = {
-                       MSG_CLIENT_AUTHENTIFICATION: self.clientAuthentification
+                       MSG_USER_AUTHENTIFICATION: self.userAuthentification
                      }
     self.factory = factory
     self.user = User(self)
 
   def connectionMade(self):
-    self.log.info("Connection established from {connection}", connection=self.transport.getPeer())
+    self.log.info("connection established from {connection}", connection=self.transport.getPeer())
     self.sendMessage(MSG_SERVER_AUTHENTIFICATION, version={"major": version.MAJOR, "minor": version.MINOR, "revision": version.REVISION}, databaseVersion=self.factory.cardsDatabaseVersion)
 
   def messageReceived(self, code, data):
+    if not self.authenticated and code!=MSG_CLIENT_AUTHENTIFICATION:
+      self.log.warn("received message {code}:{message} before actual client authentification", code=code, message=data)
+      return
     if not code in self.callbacks:
-      self.log.warn("Received unsupported message {message} with code {code} from {connection}", message=data, connection=self.transport.getPeer(), code=code)
+      self.log.warn("received unsupported message {message} with code {code} from {connection}", message=data, connection=self.transport.getPeer(), code=code)
     else:
       self.callbacks[code](**data)
 
-  def clientAuthentification(self, username, password):
+  def userAuthentification(self, username, password):
+    if len(username)>30 or len(password)!=128:
+      self.sendMessage(MSG_USER_LOGIN, success=False)
+      return
     if self.user.exists():
-      self.sendMessage(MSG_CLIENT_LOGIN, success=self.user.login(username, password))
+      self.sendMessage(MSG_USER_LOGIN, success=self.user.login(username, password))
     else:
-      self.sendMessage(MSG_CLIENT_REGISTRATION, success=self.user.register(username, password))
+      self.sendMessage(MSG_USER_REGISTRATION, success=self.user.register(username, password))
