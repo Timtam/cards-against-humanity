08e53413e380e7dd54159e9484fed55c8a97d991
MX795 <maxha95@web.de>
2017-04-11 23:14:34 +0200

    added splitter to MainFrame
    MainFrame split into panel for cardlist and panel for current card

--- card-editor/main.py
+++ card-editor/main.py
@@ -3,35 +3,65 @@
 
 import wx
 
+APP_EXIT = 1
 
-class MainWindow(wx.Frame):
-  def __init__(self, *args, **kwargs):
-    super(MainWindow, self).__init__(*args, **kwargs)
+
+class CardListPanel(wx.Panel):
+  def __init__(self, parent):
+    wx.Panel.__init__(self, parent=parent)
+
+    self.SetBackgroundColour("white")
+
+
+class CurrCardPanel(wx.Panel):
+  def __init__(self, parent):
+    wx.Panel.__init__(self, parent=parent)
+
+    self.SetBackgroundColour("white")
+
+
+class MainFrame(wx.Frame):
+  def __init__(self):
+    wx.Frame.__init__(self, None, title="Card Editor",
+                      size=(1280, 720))
 
     self.initUI()
+    self.Centre()
+
+    splitter = wx.SplitterWindow(self,
+                                 style=wx.SP_LIVE_UPDATE)
+    left_panel = CardListPanel(splitter)
+    right_panel = CurrCardPanel(splitter)
+
+    # split the window
+    splitter.SplitVertically(left_panel, right_panel, 320)
+    splitter.SetMinimumPaneSize(160)
+    splitter.SetSashGravity(0.5)
+
 
   def initUI(self):
     menubar = wx.MenuBar()
     file_menu = wx.Menu()
-    menu_item = file_menu.Append(wx.ID_EXIT, 'Quit',
-                                 'Quit application')
+    # menu_item = file_menu.Append(wx.ID_EXIT, 'Quit',
+    #                              'Quit application')
+    menu_item = wx.MenuItem(file_menu, APP_EXIT,
+                            '&Quit\tCtrl+Q')
+    file_menu.AppendItem(menu_item)
+
+    self.Bind(wx.EVT_MENU, self.onQuit, id=APP_EXIT)
     menubar.Append(file_menu, '&File')
     self.SetMenuBar(menubar)
 
-    self.Bind(wx.EVT_MENU, self.onQuit, menu_item)
-
-    self.SetSize((1280, 720))
-    self.SetTitle('Card-Editor')
-    self.Centre()
-    self.Show(True)
+    # self.Bind(wx.EVT_MENU, self.onQuit, menu_item)
 
   def onQuit(self, e):
     self.Close()
 
 
 def main():
-  app = wx.App()
-  MainWindow(None)
+  app = wx.App(False)
+  frame = MainFrame()
+  frame.Show()
   app.MainLoop()
 
 
