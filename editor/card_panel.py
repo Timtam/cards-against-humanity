2d6900dd6dc5145b61e031b8daa97f52057bb9bc
MX795 <maxha95@web.de>
2017-04-27 01:00:13 +0200

    added hover effect to CardPanel
    background color of panel changing when you enter the the panel or text field, badly the background color will be set to original when you leave the text field and not when you leave the panel (no hover effect when you move from text to panel, but there are just 3 pixels of panel between text and free space)

--- editor/card_panel.py
+++ editor/card_panel.py
@@ -20,20 +20,35 @@ class CardPanel(wx.Panel):
       self.text.SetBackgroundColour("white")
       self.text.SetForegroundColour("black")
 
-    self.box.Add(self.text, 1, wx.ALL | wx.EXPAND, 10)
+    self.box.Add(self.text, 1, wx.ALL | wx.EXPAND, 3)
     self.SetSizer(self.box)
 
+
+    self.color = self.GetBackgroundColour()
+
     self.Bind(wx.EVT_LEFT_UP, self.onClick)
+    self.text.Bind(wx.EVT_LEFT_UP, self.onClick)
+    self.Bind(wx.EVT_ENTER_WINDOW, self.onEntering)
+    #self.Bind(wx.EVT_LEAVE_WINDOW, self.onLeaving)
+    self.text.Bind(wx.EVT_ENTER_WINDOW, self.onEntering)
+    self.text.Bind(wx.EVT_LEAVE_WINDOW, self.onLeaving)
 
   def onClick(self, event):
-    print(self.GetName())
+    print("clicked on " + self.GetName())
+
+  def onEntering(self, event):
+    print("entered " + self.GetName())
+    self.SetBackgroundColour("red")
+    self.Refresh()
+
+  def onLeaving(self, event):
+    print("leaved " + self.GetName())
+    self.SetBackgroundColour(self.color)
+    self.Refresh()
 
 
 class CardText(wx.StaticText):
   def __init__(self, parent, text):
     wx.StaticText.__init__(self, parent=parent, label=text)
     # self.SetBackgroundColour("grey") #(debug)
-    self.Bind(wx.EVT_LEFT_UP, self.onClick)
 
-  def onClick(self, event):
-    self.Parent.onClick(wx.EVT_LEFT_UP)
