adf732c890e5846c1327a7b2a398b52de25ac5d5
Toni Barth <software@satoprogs.de>
2017-04-30 16:45:44 +0200

    bugfix for closeIntervention and new, unchanged and unapplied cards

--- editor/main.py
+++ editor/main.py
@@ -166,7 +166,9 @@ class MainFrame(wx.Frame):
         e.Veto(True)
         return
       elif result == wx.ID_NO:
-        self.right_window.setCard(self.right_window.related_card)
+        if self.right_window.related_card.getCardText() == '':
+          self.database.cursor().execute('DELETE FROM cards WHERE id = ?', (self.right_window.related_card.id, ))
+        self.right_window.Disable()
 
     if self.unsaved_changes:
       result = self.Message(caption="unsaved changes", text="You changed some cards without saving. Do you want us to save for you?", style=MSG_YES_NO)
