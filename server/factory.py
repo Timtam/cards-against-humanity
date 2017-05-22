2929f3b70919fe7ed47572f6236c31cb1f7eb09a
Toni Barth <software@satoprogs.de>
2017-05-22 20:12:53 +0200

    added password support for games

--- server/factory.py
+++ server/factory.py
@@ -20,7 +20,7 @@ class ServerFactory(Factory):
     self.serverDatabase = sqlite3.connect(os.path.join(getScriptDirectory(), "server.db"))
     cursor = self.serverDatabase.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS 'users' ('id' INTEGER PRIMARY KEY, 'name' VARCHAR(30), 'password' CHAR(128))")
-    cursor.execute("CREATE TABLE IF NOT EXISTS 'games' ('id' BIGINT NOT NULL, 'players' TEXT, 'cards' TEXT, 'database_hash' CHAR(128))")
+    cursor.execute("CREATE TABLE IF NOT EXISTS 'games' ('id' BIGINT NOT NULL, 'players' TEXT, 'cards' TEXT, 'password_hash' CHAR(128), 'database_hash' CHAR(128))")
     self.serverDatabase.commit()
     self.log.info("Loaded server database")
 
