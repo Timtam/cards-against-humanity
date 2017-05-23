72b7ab3bbc9313fec07de6a8772404a7b69c45bb
MX795 <maxha95@web.de>
2017-05-23 22:27:31 +0200

    added scrollbar to ScrolledTextPanel, but it will be shown anytime and functionality is not tested

--- client/scrolled_text_panel.py
+++ client/scrolled_text_panel.py
@@ -6,6 +6,7 @@ import pygame.locals as pl
 
 from shared.path import getScriptDirectory
 
+SCROLLBAR_THICKNESS = 20
 
 
 class ScrolledTextPanel:
@@ -15,7 +16,7 @@ class ScrolledTextPanel:
     self.screen = screen
     self.x = x
     self.y = y
-    self.width = width
+    self.width = width - SCROLLBAR_THICKNESS
     self.height = height
     self.font = pygame.font.Font(
       os.path.join(getScriptDirectory(), 'assets', 'helvetica-bold.ttf'), 16)
@@ -24,6 +25,15 @@ class ScrolledTextPanel:
     # for now only used to memorize the text the visually impaired user
     # is currently looking at
     self.line_cursor = 0
+    self.getHeight()
+    self.surface = pygame.Surface((self.width, self.height))
+    self.surface_rect = self.surface.get_rect()
+    self.ratio = 1.0
+    self.track = pygame.Rect(self.surface_rect.right - SCROLLBAR_THICKNESS + self.x, self.surface_rect.top + self.y,
+                        SCROLLBAR_THICKNESS, self.surface_rect.height)
+    self.knob = pygame.Rect(self.track)
+    self.knob.height = self.track.height * self.ratio
+    self.scrolling = False
     
   
   @staticmethod
@@ -74,7 +84,17 @@ class ScrolledTextPanel:
     for i in range(length, len(self.text_lines)):
       self.text_surfaces.append(self.font.render(self.text_lines[i], 1, (0, 0, 0)))
     self.line_cursor = len(self.text_lines)-1
-  
+
+    self.getHeight()
+    self.surface = pygame.Surface((self.width, self.height))
+    self.surface_rect = self.surface.get_rect()
+    self.ratio = (1.0 * self.surface_rect.height) / self.surface.get_height()
+    self.track = pygame.Rect(self.surface_rect.right - SCROLLBAR_THICKNESS + self.x, self.surface_rect.top + self.y,
+                        SCROLLBAR_THICKNESS, self.surface_rect.height)
+    self.knob = pygame.Rect(self.track)
+    self.knob.height = self.track.height * self.ratio
+    self.scrolling = False
+    
 
   def clearText(self):
     self.text_surfaces = []
@@ -116,16 +136,37 @@ class ScrolledTextPanel:
           speech = False
         if speech:
           display.view.speak(self.text_lines[self.line_cursor], True)
+
+    if event.type == pygame.MOUSEMOTION and self.scrolling:
+  
+      if event.rel[1] != 0:
+        move = max(event.rel[1], self.track.top - self.knob.top)
+        move = min(move, self.track.bottom - self.knob.bottom)
+    
+        if move != 0:
+          self.knob.move_ip(0, move)
+
+    elif event.type == pygame.MOUSEBUTTONDOWN and self.knob.collidepoint(event.pos):
+      self.scrolling = True
+
+    elif event.type == pygame.MOUSEBUTTONUP:
+      self.scrolling = False
+  
   
   def update(self):
     pass
   
   
   def render(self):
-    pos_y = self.y
+    self.surface.fill((255, 255, 255))
+    pos_y = 0
     for text_line in self.text_surfaces:
-      self.screen.blit(text_line, (self.x, pos_y))
+      self.surface.blit(text_line, (0, pos_y))
       pos_y += text_line.get_height()
+    #self.screen.blit(self.surface, (self.x, self.y))
+    self.screen.blit(self.surface, (self.x, (self.knob.top / self.ratio - self.y) * -1 + self.y))
+    pygame.draw.rect(self.screen, (192, 192, 192), self.track, 0)
+    pygame.draw.rect(self.screen, (0, 0, 0), self.knob.inflate(-2, -6), 3)
 
 
   def setFocus(self, value):
