0eabe6c4e610260e18256a7691b0de49c7dc227d
Toni Barth <software@satoprogs.de>
2017-05-28 16:20:28 +0200

    first works towards game saving

--- server/game.py
+++ server/game.py
@@ -11,6 +11,7 @@ class Game(object):
     self.name = name
     self.open = True
     self.password_hash = password_hash
+    self.running = False
     self.users = []
     self.white_cards = []
 
@@ -25,11 +26,22 @@ class Game(object):
     random.shuffle(self.white_cards)
 
   def mayJoin(self, user):
+    if self.running:
+      return self.formatted(success = False, message = 'game already running')
     if self.open:
       if len(self.users) + 1 > self.factory.card_database.max_players_per_game:
         return self.formatted(join=False, message='no more players allowed', password=self.password_hash != None)
       return self.formatted(join=True, password=self.password_hash != None)
-    return self.formatted(join=False, message='game not open to join')
+    else:
+      possible_users = [u for u in self.users if u['user'] == user.id]
+
+      if len(possible_users) != 1:
+        return self.formatted(success = False, message = 'you are no member of this paused game')
+
+      if possible_users[0]['joined']:
+        return self.formatted(success = False, message = 'already joined this game')
+
+      return self.formatted(success = True)
 
   def join(self, user, password):
     joinable = self.mayJoin(user)
@@ -37,12 +49,13 @@ class Game(object):
       return self.formatted(success=False, message=joinable['message'])
     if joinable['password'] and password != self.password_hash:
       return self.formatted(success=False, message='wrong password supplied')
-    possible_users = [u for u in self.users if u['user'] == user.id]
 
-    if len(possible_users)>0:
-      return self.formatted(success=False, message='already joined this game')
+    if self.open:
+      self.users.append(self.userdict(user))
+    else:
+      possible_users = [u for u in self.users if u['user'] == user.id and not u['joined']]
+      possible_users[0]['joined'] = True
 
-    self.users.append(self.userdict(user))
     return self.formatted(success=True, game_id = self.id)
 
   def getAllUsers(self):
@@ -52,21 +65,23 @@ class Game(object):
     if len(self.getAllUsers())<3:
       return self.formatted(success=False, message='not enough players in this game')
 
-    if not self.open:
+    if self.running:
       return self.formatted(success=False, message='already running')
 
+    # if the game is currently open, we need to shuffle the users
+    if self.open:
+      random.shuffle(self.users)
+      self.open = False
+
     self.open = False
+    self.running = True
 
     # all users need to get 10 cards
     for i in range(len(self.users)*10):
       self.users[i/10]['white_cards'].append(self.white_cards[i])
     self.white_cards=self.white_cards[len(self.users)*10:]
 
-    # and we need to shuffle all users and determine the one
-    # with the black card
-
-    random.shuffle(self.users)
-
+    # determine the one with the black card
     # index at 0 will always be the czar
     # and black_cards 0 will always be the current black card
 
