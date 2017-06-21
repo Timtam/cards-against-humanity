dbbdb9a556a35fdcb7dd434cf2140d59cab15ed5
Toni Barth <software@satoprogs.de>
2017-06-21 19:33:32 +0200

    added first translator object implementation (not tested yet!) and linked it into the client/display.py

--- client/display.py
+++ client/display.py
@@ -14,6 +14,7 @@ from .connection_view import ConnectionView
 from .message_view import MessageView
 from .game_view import GameView
 from .overview_view import OverviewView
+from shared.translator import Translator
 from shared.path import getScriptDirectory
 
 
@@ -24,6 +25,7 @@ class Display(object):
     self.accessibility = accessibility
     self.endpoint = None
     self.factory = ClientFactory(self)
+    self.translator = Translator('client')
     # initializing the loop caller
     self.loop = LoopingCall(self.process)
     self.reactor = None
@@ -86,6 +88,7 @@ class Display(object):
   def stop(self):
     pygame.quit()
     self.reactor.stop()
+    self.translator.close()
     self.running = False
   
   
---
+++ shared/translator.py
@@ -0,0 +1,77 @@
+from .path import getScriptDirectory
+
+import lxml.etree as ET
+import os
+import os.path
+
+class Translator(object):
+
+  def __init__(self, node):
+    self.node = node
+    self.init()
+
+  def getAvailableLanguages(self):
+    return [self.getDefaultLanguage()]+[os.path.splitext(lang)[0] for lang in os.listdir(self.getLanguageFolder())]
+
+  def getDefaultLanguage(self):
+    return 'English'
+
+  def getLanguageFolder(self):
+    return os.path.join(getScriptDirectory(), 'languages')
+
+  def setLanguage(self, language):
+    self.close()
+    if language != self.getDefaultLanguage() and language in self.getAvailableLanguages():
+      self.language = language
+      xml = self.openXml(os.path.join(self.getLanguageFolder(), language+'.xml'))
+      self.xml = xml.getroot().xpath(self.node)[0]
+      if not os.path.exists(os.path.join(getScriptDirectory(), 'untranslated')):
+        os.makedirs(os.path.join(getScriptDirectory(), 'untranslated'))
+      self.untranslated_xml = self.openXml(os.path.join(getScriptDirectory(), 'untranslated', language+'.xml'))
+
+  def getLanguage(self):
+    return self.language
+
+  def translate(self, string):
+
+    if self.xml is not None:
+      translations = self.xml.xpath('./string/origin[text() = "'+string+"']/../translation")
+      if len(translations)>0:
+        return translations[0].text
+      else:
+        # untranslated strings will be stored inside a xml document
+        node = self.untranslated_xml.getroot().xpath(self.node)[0]
+        if len(node.xpath('string/origin[text() = "'+string+'"]/../translation'))==0:
+          element = ET.SubElement(node, "string")
+          origin = ET.SubElement(element, "origin")
+          origin.text = string
+          translation = ET.SubElement(element, "translation")
+          translation.text = string
+
+    return string
+
+  def close(self):
+    if self.untranslated_xml is not None and len(self.untranslated_xml.getroot().xpath(self.node)[0])>0:
+      # we got some untranslated strings
+      # we will store them inside a file
+      self.untranslated_xml.write(os.path.join(getScriptDirectory(), 'untranslated', self.language+'.xml'), pretty_print = True, encoding = 'utf-8')
+    self.init()
+
+  def init(self):
+    self.language = self.getDefaultLanguage()
+    self.untranslated_xml = None
+    self.xml = None
+
+  def openXml(self, path):
+    if not os.path.exists(path):
+      element = ET.Element('translations')
+      xml = ET.ElementTree(element)
+      xml.docinfo.xml_version = 1.0
+      ET.SubElement(element, self.node)
+    else:
+      xml = ET.parse(path)
+      element = xml.getroot().xpath(self.node)
+      if len(element) == 0:
+        element = ET.SubElement(xml.getroot(), self.node)
+
+    return xml
